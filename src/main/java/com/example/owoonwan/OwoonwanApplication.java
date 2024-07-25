96ce34526911664c906fc4fed06f798ade44ecb4
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-07-25 16:11:36 +0900

    feat:Add request_template

---
+++ src/main/java/com/example/owoonwan/OwoonwanApplication.java
@@ -0,0 +1,13 @@
+package com.example.owoonwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwoonwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwoonwanApplication.class, args);
+	}
+
+}
