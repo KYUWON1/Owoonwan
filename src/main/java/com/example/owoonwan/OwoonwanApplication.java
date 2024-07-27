41c6103be924d9a732175f88bc88ceb712fb88b1
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-07-27 13:39:07 +0900

    feat:README update

--- src/main/java/com/example/owoonwan/OwoonwanApplication.java
+++ src/main/java/com/example/owoonwan/OwoonwanApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OwoonwanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OwoonwanApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OwoonwanApplication.class, args);
+    }
 
 }
